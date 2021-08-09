02e6cd17ac9caa259f7e47594eddcd56748f51d8
mounika <325mounika@gmail.com>
2021-08-09 04:09:40 +0000

    this is first code

---
+++ file1.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+region = "us-east-2"
+access_key = ""
+secret_key = ""
+}
+resource "aws_instance" "trlinux" {
+ami = "ami-0443305dabd4be2bc"
+instance_type = "t2.micro"
+key_name = "wbg28"
+count = "2"
+tags = {
+        Name = "terraform"
+
+}
+}
